3c2be83cd4d39f3bc62af5bea87a58d9c216cfae
marivishwan <shanmugamariyappan@gmail.com>
2022-09-05 17:01:05 +0530

    add the file

---
+++ obj.js
@@ -0,0 +1,159 @@
+
+let earpods={
+    brand: "oneplus",
+    model: z2,
+    color: "black",
+    charging_time: 1,
+    playback_time:  3,
+} 
+console.log(earpods);
+console.log(earpods.brand);
+
+let notebook={
+    brand: "Diamond",
+    type: "double side ruled ",
+    total_pages: 150,
+}
+console.log(notebook);
+console.log(notebook["total_pages"]);
+
+let pen={
+    color: " blue and white",
+    ink_color:"Blue",
+    style: "with cap"
+}
+console.log(pen);
+console.log(pen["style"]);
+
+let switchboard={
+    material: "Plastic",
+    socket: 4,
+    switches: 2,
+}
+console.log(switchboard);
+console.log(switchboard["socket"]);
+
+let tubelight={
+    brand: "Orbit",
+    type: "LED",
+    color: "white",
+    power:"50 watts"
+}
+console.log(tubelight);
+console.log(tubelight["type"]);
+
+let plywood={
+    length:40,
+    thickness:5,
+    style: "with Mica",
+}
+console.log(plywood);
+console.log(plywood["style"]);
+
+let bag={
+    brand : "Roshan",
+    capacity:20,
+    style: "backbag", 
+}
+console.log(bag);
+console.log(bag["style"]);
+
+let tshirt={
+    color: "Blue",
+    collar_type:"Chinese",
+    material: "Demin"
+}
+console.log(tshirt);
+console.log(tshirt["material"]);
+
+let cake={
+    flavour: "Blackcurrent",
+    kg: 2,
+    shape: "round",
+}
+console.log(cake);
+console.log(cake["flavour"]);
+
+let aeroplane={
+    company: "airbus",
+    passenger_capacity: 600,
+    ticket_prize: 13000,
+    seat: "1st"
+} 
+console.log(bus);
+console.log(aeroplane.company);
+
+let water_bottle={
+    material: "Steel",
+    capacity: "1 Litre",
+}
+console.log(water_bottle);
+console.log(water_bottle.capacity);
+
+let song={
+    hit:"meloady",
+    language:"tamil",
+    writer:"vairamuthu",
+    music: "ilayaraja",
+}
+console.log(song);
+console.log(song.hit);
+
+let pencil={
+    color: 25,
+    company: "craiyance",
+    size: "medlarge",
+    prize: 25,
+}
+console.log(pencil);
+console.log(pencil.company);
+
+let fiber={
+    company:"jio",
+    band: 16,
+    users: 4, 
+}
+console.log(fiber);
+console.log(fiber.band);
+
+let laptop={
+    company: "asus",
+    Watts: 42,
+    core: "i3",
+    prize: 45000,
+}
+console.log(laptop);
+console.log(laptop.company);
+
+let chain={
+    material: "gold",
+    style: "classic",
+    carrect: 22,
+}
+console.log(chain);
+console.log(chain.carract);
+
+let mobilephone={
+    RAM: 16,
+    memary: 128,
+    model: "8pro",
+    company: "realme",
+}
+console.log(mobilephone);
+console.log(mobilephone.Comp);
+
+let lock={
+    type: "Lever",
+    Lever: 6,
+    material: "Steel",
+}
+console.log(lock);
+console.log(lock.material);
+
+let cupboard={
+    plate : 4,
+    door: 2,
+    Wood: "plaine"
+}
+console.log(cupboard);
+console.log(cupboard.Wood);
